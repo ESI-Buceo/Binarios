53fd90e648c2acee3acbb31190275442eccab051
SebatheDragonbear <45465292+SebatheDragonbear@users.noreply.github.com>
2020-10-15 07:57:34 -0300

    Add files via upload

---
+++ Programación/Projects/Gestion/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+
+    Private Sub MaskedTextBox1_MaskInputRejected(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MaskInputRejectedEventArgs) Handles UsuarioText.MaskInputRejected
+
+    End Sub
+
+    Private Sub MaskedTextBox2_MaskInputRejected(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MaskInputRejectedEventArgs) Handles ContraseñaText.MaskInputRejected
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim usuario, usuariop, contrap, contra As String
+        usuario = UsuarioText.Text
+        contra = ContraseñaText.Text
+        UsuarioText.Text = usuario
+        ContraseñaText.Text = contra
+
+        usuariop = "pepe"
+        contrap = "1234"
+
+        If (UsuarioText.Text = "" And Not ContraseñaText.Text = "") Then
+            MessageBox.Show("ingresa el usuario", "error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            UsuarioText.Focus()
+
+        ElseIf (Not UsuarioText.Text = "" And ContraseñaText.Text = "") Then
+            MessageBox.Show("ingrese su contraseña", "error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            UsuarioText.Focus()
+
+        ElseIf (UsuarioText.Text = "" And ContraseñaText.Text = "") Then
+            MessageBox.Show("debes llenar los campos", "error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+            UsuarioText.Focus()
+
+        
+
+        ElseIf UsuarioText.Text = usuariop And ContraseñaText.Text = contrap Then
+            Form2.Show()
+            Me.Hide()
+        
+
+
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ Programación/Projects/Gestion/Form2.vb
@@ -0,0 +1,172 @@
+﻿Imports System.IO
+
+Public Class Form2
+
+    Private Property archivo As IO.FileStream
+
+
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SintomasText.SelectedIndexChanged
+
+        
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim Enfermedad As String
+
+        Enfermedad = Enfermedadtxt.Text
+
+        ' Dim textoANT As String = My.Computer.FileSystem.ReadAllText("C:\" + Enfermedad + ".txt")
+
+
+
+        'Dim obj As Object
+        'Dim archivo As Object
+        ' obj = CreateObject("Scripting.FileSystemObject")
+        'archivo = obj.CreateTextFile("C:\" + Enfermedad + ".txt")
+
+
+
+        Dim i As Integer
+        For i = 0 To SintomasText.Items.Count - 1
+            If (SintomasText.GetItemChecked(i)) Then
+                MessageBox.Show(SintomasText.Items(i))
+            End If
+        Next
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        End
+
+
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        'Dim ruta As String = My.Application.Info.DirectoryPath
+        Dim textoANT As String = My.Computer.FileSystem.ReadAllText("C:\Sintomas.txt")
+        Dim textoNUE As String = SintomasTxt.Text
+
+
+
+        Dim obj As Object
+        Dim archivo As Object
+        obj = CreateObject("Scripting.FileSystemObject")
+        archivo = obj.CreateTextFile("C:\Sintomas.txt")
+        archivo.WriteLine(textoANT)
+        archivo.WriteLine(textoNUE)
+        archivo.Close()
+
+        SintomasText.Items.Clear()
+
+
+
+        Dim objLectorList As StreamReader = File.OpenText("C:\Sintomas.txt")
+        Do While Not objLectorList.EndOfStream
+
+            Me.SintomasText.Items.Add(objLectorList.ReadLine)
+        Loop
+        FileClose()
+        objLectorList.Close()
+
+
+
+    End Sub
+
+
+
+
+
+
+
+
+
+
+
+    Private Sub Label3_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub MaskedTextBox2_MaskInputRejected(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MaskInputRejectedEventArgs) Handles SintomasTxt.MaskInputRejected
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Dim indice As Integer = SintomasText.Items.Count - 2
+
+        Dim i As Integer
+
+        For i = 0 To indice
+            If (SintomasText.GetItemChecked(i)) Then
+                SintomasText.Items.RemoveAt(i)
+                indice = indice - 1
+            End If
+        Next
+
+        Dim obj As Object
+        Dim archivo As Object
+        obj = CreateObject("Scripting.FileSystemObject")
+        archivo = obj.CreateTextFile("C:\Sintomas.txt")
+        archivo.WriteLine(textoANT)
+        archivo.WriteLine(textoNUE)
+        archivo.Close()
+
+        SintomasText.Items.Clear()
+
+    End Sub
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Dim objLectorList As StreamReader = File.OpenText("C:\Sintomas.txt")
+        Do While Not objLectorList.EndOfStream
+            Me.SintomasText.Items.Add(objLectorList.ReadLine)
+        Loop
+        FileClose()
+        objLectorList.Close()
+
+    End Sub
+
+    Private Function pepe() As String
+        Throw New NotImplementedException
+    End Function
+
+    Private Sub CheckedListBox1_SelectedIndexChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SintomasText.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub SintomasTxt_MaskInputRejected(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MaskInputRejectedEventArgs) Handles SintomasTxt.MaskInputRejected
+
+    End Sub
+
+    Private Sub Sintomas_MaskInputRejected(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MaskInputRejectedEventArgs) Handles Enfermedadtxt.MaskInputRejected
+
+    End Sub
+
+    Private Function EliminarSaltosLinea(ByVal texto As String, ByVal caracterReemplazar As String) As String
+        EliminarSaltosLinea = Replace(Replace(texto, Chr(10), caracterReemplazar), Chr(13), caracterReemplazar)
+    End Function
+
+End Class
