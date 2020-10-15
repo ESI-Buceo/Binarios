4b8a971e1cbd189a81f0d13c42c540e3e4580027
SebatheDragonbear <45465292+SebatheDragonbear@users.noreply.github.com>
2020-10-15 08:07:16 -0300

    Add files via upload

---
+++ Programación/Projects/sintomas/sintomas/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Idiomas = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ContraseñaText = New System.Windows.Forms.MaskedTextBox()
+        Me.UsuarioText = New System.Windows.Forms.MaskedTextBox()
+        Me.SuspendLayout()
+        '
+        'Idiomas
+        '
+        Me.Idiomas.FormattingEnabled = True
+        Me.Idiomas.Items.AddRange(New Object() {"", "Español", "Ingles", "Portugues", "Frances"})
+        Me.Idiomas.Location = New System.Drawing.Point(1, 1)
+        Me.Idiomas.Name = "Idiomas"
+        Me.Idiomas.Size = New System.Drawing.Size(121, 21)
+        Me.Idiomas.TabIndex = 11
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(243, 144)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Intoducir contraseña"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(243, 91)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Intoducir usuario"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(246, 214)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(101, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "ingresar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ContraseñaText
+        '
+        Me.ContraseñaText.Location = New System.Drawing.Point(246, 160)
+        Me.ContraseñaText.Name = "ContraseñaText"
+        Me.ContraseñaText.Size = New System.Drawing.Size(100, 20)
+        Me.ContraseñaText.TabIndex = 7
+        '
+        'UsuarioText
+        '
+        Me.UsuarioText.Location = New System.Drawing.Point(246, 107)
+        Me.UsuarioText.Name = "UsuarioText"
+        Me.UsuarioText.Size = New System.Drawing.Size(100, 20)
+        Me.UsuarioText.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(619, 388)
+        Me.Controls.Add(Me.Idiomas)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ContraseñaText)
+        Me.Controls.Add(Me.UsuarioText)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Idiomas As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ContraseñaText As System.Windows.Forms.MaskedTextBox
+    Friend WithEvents UsuarioText As System.Windows.Forms.MaskedTextBox
+End Class
---
+++ Programación/Projects/sintomas/sintomas/Form2.vb
@@ -0,0 +1,75 @@
+﻿Imports System.IO
+
+Public Class Form2
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        'dim es para crear variables'
+
+        Dim Fiebre As Boolean, tos_humeda As Boolean, Dolor_garganta As Boolean, Goteo As Boolean, Dolores_musculares As Boolean, Dolores_cabeza As Boolean, fatiga As Boolean, Tos_seca As Boolean
+
+        Fiebre = SintomasText.GetItemChecked(0)
+        tos_humeda = SintomasText.GetItemChecked(1)
+        Dolor_garganta = SintomasText.GetItemChecked(2)
+        Goteo = SintomasText.GetItemChecked(3)
+        Dolores_musculares = SintomasText.GetItemChecked(4)
+        Dolores_cabeza = SintomasText.GetItemChecked(5)
+        fatiga = SintomasText.GetItemChecked(6)
+        Tos_seca = SintomasText.GetItemChecked(7)
+
+
+        If ((Fiebre And tos_humeda And Dolor_garganta And Goteo And Dolores_musculares And Dolores_cabeza And fatiga And Not Tos_seca)) Then
+
+            MessageBox.Show("tienes gripe")
+
+        ElseIf (Fiebre And Tos_seca And fatiga And Not tos_humeda And Not Dolor_garganta And Not Goteo And Not Dolores_musculares And Not Dolores_cabeza) Then
+            MessageBox.Show("Posiblemente tienes coronavirus")
+        End If
+
+
+    End Sub
+
+    Private Sub CheckedListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub CheckedListBox1_SelectedIndexChanged_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SintomasText.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Form1.Show()
+    End Sub
+
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Dim objLectorList As StreamReader = File.OpenText("C:\Sintomas.txt")
+        Do While Not objLectorList.EndOfStream
+
+            Me.SintomasText.Items.Add(objLectorList.ReadLine)
+        Loop
+
+        FileClose()
+        objLectorList.Close()
+
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
